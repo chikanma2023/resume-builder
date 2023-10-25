1e5015334f9dccf99dde5cf91ae75f889b4a0fc2
Hart102 <hartongit102@gmail.com>
2023-10-25 15:47:12 -0400

    worked on sections component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import ".././src/dist/output.css";
-import "././dist/output.css";
+import "./dist/output.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
